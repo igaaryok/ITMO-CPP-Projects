57cfea68c9cc0c9db34937205ec5347c8bfc64aa
Igor <igormatevosian@gmail.com>
2022-05-23 11:00:33 +0300

    PoolAllocator ended

--- Allocator/main.cpp
+++ Allocator/main.cpp
@@ -5,11 +5,53 @@
 #include "PoolAllocator.h"
 
 #include <iostream>
-#include <fstream>
-#include <cstdlib>
+#include <vector>
+#include <chrono>
+#include <map>
 
-int main(int argc, char *argv[])
-{
-    std::cout << "hello world";
+long long myAllocatorTest(int n) {
+    auto start = std::chrono::steady_clock::now();
+    std::vector<int, PoolAllocator<int>> vec;
+
+    for (int i = 0; i < n; ++i) {
+        vec.push_back(i);
+    }
+
+    auto end = std::chrono::steady_clock::now();
+
+    auto result = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    return result.count();
+}
+
+long long stdAllocatorTest(int n) {
+    auto start = std::chrono::steady_clock::now();
+    std::vector<int> vec;
+
+    for (int i = 0; i < n; ++i) {
+        vec.push_back(i);
+    }
+
+    auto end = std::chrono::steady_clock::now();
+
+    auto result = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+
+    return result.count();
+}
+
+int main(int argc, char *argv[]) {
+    int n = 100000;
+
+    std::map<int, PoolAllocator<int>> mapa;
+
+    mapa[0] = 0;
+    mapa[1] = 1;
+    mapa[2] = 2;
+    mapa[3] = 3;
+    mapa[4] = 4;
+
+    std::cout << "Time is: " << myAllocatorTest(n) << " microseconds\n";
+
+    std::cout << "Time is: " << stdAllocatorTest(n) << " microseconds\n";
     return 0;
 }
