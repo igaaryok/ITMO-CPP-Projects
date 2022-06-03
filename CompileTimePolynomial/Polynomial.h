491f37c87486e7a063e79465f3f2c0a0dc6b6423
Igor <igormatevosian@gmail.com>
2022-06-03 23:22:16 +0300

    Beginning of compile time polynomial

---
+++ CompileTimePolynomial/Polynomial.h
@@ -0,0 +1,14 @@
+//
+// Created by Igor on 03.06.2022.
+//
+
+#ifndef ITMO_CPP_PROJECTS_POLYNOMIAL_H
+#define ITMO_CPP_PROJECTS_POLYNOMIAL_H
+
+
+class Polynomial {
+
+};
+
+
+#endif //ITMO_CPP_PROJECTS_POLYNOMIAL_H
---
+++ CompileTimePolynomial/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Igor on 03.06.2022.
+//
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
